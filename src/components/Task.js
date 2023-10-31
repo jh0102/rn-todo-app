7243ce35b3fa2d14992ed50e446677c9cd9451b2
jh0102 <halo0102@naver.com>
2023-10-31 18:20:53 +0900

    리액네 투두앱 제작

---
+++ src/components/Task.js
@@ -0,0 +1,67 @@
+import React, {useState} from "react";
+import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
+import IconButton from "./iconButton";
+import { icons } from "../Icons";
+import Input from "./Input";
+
+const Container  =styled.View`
+    flex-direction: row;
+    align-items: center;
+    background-color: ${({theme})=> theme.itemBackground};
+    border-radius: 10px;
+    padding: 5px;
+    margin: 3px 0;
+`;
+const Contents = styled.Text`
+    flex: 1;
+    font-size: 24px;
+    color: ${({theme, completed})=> (completed ? theme.done : theme.text)};
+    text-decoration-line: ${({ completed }) => completed ? 'line-through' : 'none'};
+`;
+
+const Task = ({item, deleteTask, toggleTask}, updateTask) => {
+    const [isEditng, setIsEditing] = useState(false);
+    const [text,setText] = useState(item.text);
+
+    const _onSubmit = () => {
+        if(isEditng){
+            const updatedItem = Object.assign({}, item);
+            updatedItem['text'] = text;
+            setIsEditing(false);
+            setIsEditing(false);
+        }
+        
+    }
+
+    return isEditng ? <Input 
+    value={text} 
+    onChangeText={text => setText(text)} 
+    onSubmitEditing={_onSubmit}
+    onBlur={() => {
+        setText(item.text);
+        setIsEditing(false);
+    }}
+    /> : (
+        <Container>
+            <IconButton
+                icon={item.completed ? icons.check: icons.uncheck}
+                item={item}
+                onPress={toggleTask}/>
+            <Contents completed={item.completed}>{item.text}</Contents>
+            {item.completed || 
+            <IconButton icon={icons.edit} onPress={() => setIsEditing(true)}/>}
+            <IconButton icon={icons.delete} item={item} onPress={deleteTask}/>
+        </Container>
+    );
+};
+
+
+Task.proptpyes = {
+    item: PropTypes.object.isRequired,
+    deleteTask: PropTypes.func.isRequired,
+    toggleTask: PropTypes.func.isRequired,
+    updateTask :PropTypes.func.isRequired,
+};
+
+export default Task;
---
+++ src/components/iconButton.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from 'styled-components/native';
+import { TouchableOpacity, View } from "react-native";
+import PropTypes from 'prop-types';
+import {icons} from '../Icons';
+
+const Icon = styled.Image`
+    width: 30px;
+    height: 30px;
+    margin: 10px;
+    tint-color: ${({ theme, completed })=> completed ? theme.done : theme.text};
+`;
+
+const IconButton = ({icon, onPress, item}) => {
+    const _onPress = () => {
+        onPress(item.id);
+    }
+    return (
+        <TouchableOpacity onPress={_onPress}>
+            <View>
+                <Icon source={icon} completed={item.completed}>
+                </Icon>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+IconButton.defaultProps = {
+    item: {completed: false}
+}
+
+IconButton.propTypes = {
+    icon: PropTypes.oneOf(Object.values(icons)).isRequired,
+    onPress: PropTypes.func,
+    item: PropTypes.object,
+}
+
+export default IconButton;
